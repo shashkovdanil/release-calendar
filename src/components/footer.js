7cb6c0e0465b69ad09bc5b3d6b4fb20618d44fc1
Danil Shashkov <shashkovdanil@gmail.com>
2020-01-07 21:25:47 +0500

    Добавил лого, пофиксил стили

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
 
 function Footer() {
   return (
@@ -6,7 +7,9 @@ function Footer() {
       <a className="vk" href="https://vk.com/deaddinos">
         vk.com/deaddinos
       </a>
-      <div className="logo">DEAD DINOS</div>
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
       <a className="tg" href="https://t.me/deaddinos">
         t.me/deaddinos
       </a>
